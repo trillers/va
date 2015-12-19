a09819bd17fe227252760ed9865f322656e6e9fb
leekangtaqi <leekangtaqi@sina.com>
2015-12-19 17:44:24 +0800

    o

--- test/drive_baidu.js
+++ test/drive_baidu.js
@@ -1,5 +1,12 @@
 var webdriver = require('selenium-webdriver');
-var driver = new webdriver.Builder()
+var chromeDriver = require('selenium-webdriver/chrome');
+var chromeCapabilities = webdriver.Capabilities.chrome();
+var options = chromeDriver.Options.fromCapabilities(chromeCapabilities);
+options.addArguments('--lang=en_US');
+options.addArguments('--disable-user-media-security=true');
+options.addArguments('--disable-extensions=true');
+options.setUserPreferences({'intl.accept_languages': 'zh_CN'});
+var driver =  new webdriver.Builder()
     .withCapabilities(options.toCapabilities())
     .setControlFlow(new webdriver.promise.ControlFlow())
     .build();
